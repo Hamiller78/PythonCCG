e64ef3b1d498e5f536f31b367d82c9e157879cd8
Torben Kneesch <Kneesch@gmx.de>
2025-09-28 11:29:41 +0300

    Add gameloop

---
+++ pythonccg/gameloop.py
@@ -0,0 +1,43 @@
+from pythonccg.cardpool import Cardpool
+from pythonccg.gamestate import Gamestate
+from pythonccg.moveprovider import MoveProvider
+from pythonccg.moves import *
+from pythonccg.zone import Zone
+import random
+
+class Gameloop:
+
+    def __init__(self):
+        pass
+
+    def setup_game(self) -> Gamestate:
+        cardpool = Cardpool()  # Assuming a CardPool class exists to manage card creation
+        cardpool.load_cards_from_file('assets/BasicSet.csv')
+
+        initial_state = Gamestate()
+        initial_state.draw_deck[0].fill_from_file('assets/Decklist.txt', cardpool)
+        initial_state.draw_deck[1].fill_from_file('assets/Decklist.txt', cardpool)
+
+        for _ in range(3):
+            initial_state.hand[0].add_to_bottom(initial_state.draw_deck[0].remove_from_top())
+            initial_state.hand[1].add_to_bottom(initial_state.draw_deck[1].remove_from_top())
+        initial_state.hand[1].add_to_bottom(initial_state.draw_deck[1].remove_from_top())
+
+        return initial_state
+
+    def run_game(self, initial_state: Gamestate) -> int:
+        current_state = initial_state
+        while True:
+            available_moves = MoveProvider().get_all_moves(initial_state)
+            selected_move = random.choice(available_moves)
+            new_state = selected_move.get_new_gamestate()
+
+            if new_state.health[0] <= 0 and new_state.health[1] <= 0:
+                print("The game is a draw!")
+                return -1  # Draw
+            elif new_state.health[0] <= 0:
+                print("Player 2 wins!")
+                return 1  # Player 2 wins
+            elif new_state.health[1] <= 0:
+                print("Player 1 wins!")
+                return 0  # Player 1 wins
---
+++ pythonccg/moves.py
@@ -0,0 +1,11 @@
+from .moveplaycard import MovePlayCard
+from .moveminionattacksminion import MoveMinionAttacksMinion
+from .moveminionattacksplayer import MoveMinionAttacksPlayer
+from .movepass import MovePass
+
+__all__ = [
+    "MovePlayCard",
+    "MoveMinionAttacksMinion",
+    "MoveMinionAttacksPlayer",
+    "MovePass",
+]
